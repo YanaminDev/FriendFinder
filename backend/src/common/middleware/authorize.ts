30e7dc0250c4dae34b452f51b9e61ec81ae46cfb
YanaminDev <thiraphat.b.yanamin@gmail.com>
2026-03-03 03:24:02 +0700

    add authorize

---
+++ backend/src/common/middleware/authorize.ts
@@ -0,0 +1,15 @@
+import {Request, Response, NextFunction} from 'express'
+
+function authorize(...allowedRoles: string[]) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (!req.user) {
+            return res.status(401).json({error : "Not authenticated"})
+        }
+        if(!allowedRoles.includes(req.user.role)){
+            return res.status(403).json({error: "Access denied. Reqires role: " + allowedRoles.join(" or ")})
+        }
+        next()
+    }
+}
+
+module.exports = {authorize}
