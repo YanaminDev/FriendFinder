c609b9b7c8c67a2b24a37b0fbc0cd199031b9387
Kittitouchhh <s66111596@gmail.com>
2026-04-08 12:18:12 +0700

    refactor: remove unnecessary info text from LocationPicker component

--- frontend/src/features/map/LocationPicker.tsx
+++ frontend/src/features/map/LocationPicker.tsx
@@ -192,10 +192,7 @@ const LocationPicker: React.FC<LocationPickerProps> = ({
         </Button>
       </div>
 
-      {/* Info Text */}
-      <div className="absolute bottom-40 left-1/2 -translate-x-1/2 bg-black bg-opacity-50 text-white px-4 py-2 rounded text-sm z-20">
-        Move the map to select a location
-      </div>
+      
     </div>
   );
 };
