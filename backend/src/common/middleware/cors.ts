a25341b8436aebe57ce853f3cc6f74ccd21b7ad8
YanaminDev <thiraphat.b.yanamin@gmail.com>
2026-04-17 04:15:44 +0700

    fixed cors

--- backend/src/common/middleware/cors.ts
+++ backend/src/common/middleware/cors.ts
@@ -1,7 +1,18 @@
 import cors from 'cors';
 
 export const corsMiddleware = cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:8081', 'http://192.168.1.100:8081', 'http://192.168.1.100:19000', 'http://192.168.1.100:19001'],
+  origin: [
+    // Development
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://localhost:8081',
+    'http://192.168.1.100:8081',
+    'http://192.168.1.100:19000',
+    'http://192.168.1.100:19001',
+    // Production
+    'https://api.friendsfinders.uk',
+    'https://friendsfinders.uk',
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
